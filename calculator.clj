c791db60a7da0157d90ff51e75384a872147561e
Malwine <malwine@mailbox.org>
2015-05-19 11:05:45 +0200

    Add sub, multiply and devide method

--- calculator.clj
+++ calculator.clj
@@ -3,4 +3,26 @@
 (defn add [a b]
 (+ a b))
 
+;;call add function
 (add 1 2)
+
+(defn sub [a b]
+(- a b))
+
+;;call substraction function
+(sub 4 8)
+
+(defn multiply [a b]
+(* a b))
+
+;;call multiply function
+(multiply 2 8)
+
+(defn divide [a b]
+  (if (not (= b 0))
+     (do (/ a b) (/ a b))
+     (do (println "Can't devide by 0!") "Can't devide by 0!" )))
+
+;;call the divide function
+(divide 6 3)
+(divide 5 0)
